9381c91c0a25d380bb17f7045b43a662a53e5c4e
김재현 <94097685+iconew123@users.noreply.github.com>
2025-05-15 05:52:14 +0900

    init commit

---
+++ src/main/java/com/examole/jikmutest/JikmutestApplication.java
@@ -0,0 +1,13 @@
+package com.examole.jikmutest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JikmutestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JikmutestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examole/jikmutest/JikmutestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examole.jikmutest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JikmutestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
